48c4640eb15cff5f02198f71f9c7d359b47f633c
Germinal56 <stockhudson@mail.com>
2024-07-07 13:27:36 +0200

    first commit

---
+++ models/User.js
@@ -0,0 +1,50 @@
+// in this file i will create a user schema in mongoose for authentication purposes
+// i will use bcrypt to hash the password before saving it to the database
+// i will use jsonwebtoken to create a token when a user logs in
+// i will use express-jwt to protect routes
+// i will create a middleware to protect routes
+
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        minLength: 3,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minLength: 6,
+    },
+});
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name:this.name }, 
+        process.env.JWT_SECRET, 
+        {expiresIn: process.env.JWT_LIFETIME});
+};
+
+UserSchema.methods.comparePasswords = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+};
+
+module.exports = mongoose.model('User', UserSchema);
