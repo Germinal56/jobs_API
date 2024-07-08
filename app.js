51231d52b46c0e590841d99e60c744ca5f35aeb1
Germinal56 <stockhudson@mail.com>
2024-07-08 11:32:30 +0200

    added swagger docs

--- app.js
+++ app.js
@@ -30,7 +30,16 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// extra packages
+
+//documentation
+const swaggerUI = require('swagger-ui-express');
+//load json file as swagger documentation
+const swaggerDocument = require('./swagger.json');
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Check the documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, { explorer: true }));
 
 // routes
 app.use('/api/v1/auth', authRouter)
